b52884cf3372569d6b8cbe7b32c59356e231def1
David Kohlbrenner <david.kohlbrenner@gmail.com>
2015-03-04 23:05:05 -0800

    added a useful run script for iperf and a checklist

---
+++ tools/iperf_run_client.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Where is iperf3?
+iperf_path=./iperf3
+#1 gigabyte to send before done
+size=1G
+#Server to connect to
+server=172.16.0.2
+#set to -R to turn on server sends to client
+reverse=-R
+
+########End Config###########
+if [ $# -eq 0 ]; then
+    echo "Usage $0 run_type(can be non-unique)"
+    exit 1
+fi
+
+mkdir -p results
+
+name=results/$1
+i=1
+if [[ -e $name.$i ]] ; then
+    while [[ -e $name.$i ]] ; do
+        let i++
+    done
+fi
+name=$name.$i
+if hash unbuffer 2>/dev/null; then
+    header=unbuffer
+else
+    header=
+    echo "You don't have unbuffer, but this is running"
+fi
+
+run="$header $iperf_path -V -n $size -c $server $reverse | tee -a $name"
+
+date | tee $name
+echo $run | tee -a $name
+eval $run
